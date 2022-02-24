1836b439b7c28ecf0b447f1b55fef1c3c3219fbb
flaviocp90 <flaviocoppola90@gmail.com>
2022-02-24 17:55:25 +0100

    fix UI

--- client/src/components/Navbar/Navbar.style.js
+++ client/src/components/Navbar/Navbar.style.js
@@ -1,18 +1,18 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-    appBar: {
-        borderRadius: 15,
-        margin: "30px 0",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      },
-      heading: {
-        fontWeight: "bolder",
-      },
-      image: {
-        marginLeft: "30px",
-      },
-    }))
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 0",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  heading: {
+    fontWeight: "bolder",
+    padding: "30px",
+  },
+  image: {
+    marginLeft: "30px",
+  },
+}));
