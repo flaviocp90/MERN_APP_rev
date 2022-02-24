62e1eb188da975a10658176788af47dda4ac76dc
flaviocp90 <flaviocoppola90@gmail.com>
2022-02-24 17:55:08 +0100

    add log in page and create components

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,9 +1,139 @@
-import React from 'react'
+import React, { useState } from "react";
+import useStyles from "./Auth.style";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+} from "@material-ui/core";
+import { GoogleLogin } from "react-google-login";
+import { useDispatch } from "react-redux";
+
+import Input from "./Input";
+import Icon from "./icon";
 
 function Auth() {
+  const classes = useStyles();
+  const [isSignUp, setIsSignUp] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const handleShowPassword = () =>
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  const switchMode = () => {
+    setIsSignUp((prevIsSignup) => !prevIsSignup);
+    handleShowPassword(false);
+  };
+  const googleSuccess = async (res) => {
+    const result = res?.profileObj;
+    const token = res?.tokenId;
+    try {
+      dispatch({type: 'AUTH', data: {result, token}})
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  const googleFailure = () => {
+    console.log("Registrazione Google fallita, riprova");
+  };
   return (
-    <div>Auth</div>
-  )
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {isSignUp ? "Iscriviti" : "Registrati"}
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignUp && (
+              <>
+                <Input
+                  name="firstname"
+                  label="Nome"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastname"
+                  label="Cognome"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignUp && (
+              <Input
+                name="confirmPassword"
+                label="Ripeti la password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            {isSignUp ? "Iscriviti" : "Registrati"}
+          </Button>
+          <GoogleLogin
+            clientId="62203933170-51je9hqqb66hd02bf7lp6sc0t4urjl1e.apps.googleusercontent.com"
+            render={(renderProps) => (
+              <Button
+                className={classes.googleButton}
+                color="primary"
+                fullWidth
+                onClick={renderProps.onClick}
+                disabled={renderProps.disabled}
+                startIcon={<Icon />}
+                variant="contained"
+              >
+                Google Sign In
+              </Button>
+            )}
+            onSuccess={googleSuccess}
+            onFailure={googleFailure}
+            cookiePolicy="single_host_origin"
+          />
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignUp
+                  ? "Hai già un account? Registrati"
+                  : "Non hai un account? Iscriviti"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 }
 
-export default Auth
+export default Auth;
