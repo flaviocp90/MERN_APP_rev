941f2fc479cf7705c5ad3094e24517e0a0eeede6
flaviocp90 <flaviocoppola90@gmail.com>
2022-03-01 09:25:14 +0100

    deploy fix

--- server/index.js
+++ server/index.js
@@ -15,15 +15,18 @@ app.use(cors());
 
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
+
 app.get("/", (req, res) => {
   res.send("Hello from API");
 });
 
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL || process.env.MONGODB_URI, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() =>
     app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
